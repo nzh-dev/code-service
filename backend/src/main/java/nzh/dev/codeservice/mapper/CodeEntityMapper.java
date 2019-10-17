efa390e80ae279a88d79b0ae0ae157a35f1b334f
Antero Peippo <antero.peippo@accenture.com>
2019-10-17 22:07:06 +0300

    Included parent ids to the mapping

--- backend/src/main/java/nzh/dev/codeservice/mapper/CodeEntityMapper.java
+++ backend/src/main/java/nzh/dev/codeservice/mapper/CodeEntityMapper.java
@@ -1,8 +1,16 @@
 package nzh.dev.codeservice.mapper;
 
 import nzh.dev.codeservice.model.Code;
+import nzh.domain.CodeRelationEntity;
 import nzh.domain.CodeEntity;
 import org.modelmapper.PropertyMap;
+import org.modelmapper.Converter;
+import java.util.Collection;
+
+import java.util.List;
+import java.util.ArrayList;
+import org.modelmapper.PropertyMap;
+import java.util.stream.Collectors;
 
 /**
  * Created by nzh-dev on 16/10/2019.
@@ -16,7 +24,7 @@ public class CodeEntityMapper extends AbstractMapper {
         getMapper().addMappings(new PropertyMap<CodeEntity, Code>() {
             @Override
             protected void configure() {
-                map(source.getValueId()).setId(null);
+                map(source.getValueId()).setValueId(null);
                 map(source.getValueText()).setValue(null);
                 map(source.getSetId()).setSetId(null);
                 map(source.getSetName()).setSetName(null);
@@ -28,7 +36,11 @@ public class CodeEntityMapper extends AbstractMapper {
      * Map CodeEntity to Code
      */
     public Code map(CodeEntity source) {
-        return getMapper().map(source, Code.class);
+        Code code = getMapper().map(source, Code.class);
+        code.setParentValueIds(source.getParentCodeRelationsByValueId().stream()
+                .map(CodeRelationEntity::getParentValueId)
+                .collect(Collectors.toList()));
+        return code;
     }
 
 }
